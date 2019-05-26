5d3df16e8244d3659331aa3837456130d9478a05
Siddhartha <syd1424794@gmail.com>
2019-05-26 23:10:54 +0530

    project files added

---
+++ idea_demo/resources/views/layouts/login-layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+   @include('includes.head')
+</head>
+<body>
+   {{-- <header class="row">
+       @include('includes.header')
+   </header> --}}
+           @yield('content')
+   {{-- <footer class="row">
+       @include('includes.footer')
+   </footer> --}}
+</body>
+</html>
---
+++ idea_demo/resources/views/pages/register.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.login-layout')
+@section('content')
+   <div class="login">
+       <div class="left-sec">
+        <div class="content">
+            <h3>Welcome To</h3>
+            <h1>Mother's Treat</h1>
+        </div>
+       </div>
+
+       <div class="right-sec">
+           <div class="login-div">
+               @if (!@empty($errors))
+                   {{$errors}}
+               @endif
+
+               <h3 class="text-center mb-4">REGISTER</h3>
+               <form action="{{route('custom.register')}}" method="post">
+                    @csrf
+                    <div class="input-div">
+                        <input type="text" placeholder="Name" name="name" value="{{old('name')}}">
+                    </div>
+
+                    <div class="input-div">
+                        <input type="email" placeholder="Email" name="email" value="{{old('email')}}">
+                    </div>
+
+                    <div class="input-div">
+                        <input type="text" placeholder="Phone" name="phone" value="{{old('phone')}}">
+                    </div>
+
+                    <div class="input-div">
+                        <input type="password" placeholder="Password" name="password" value="{{old('password')}}">
+                    </div>
+
+                    <div class="input-div">
+                        <button type="submit">Register <i class="fa fa-long-arrow-right"></i></button>
+                    </div>
+                </form>
+
+                <div class="links">
+                    <p>Already have an account?
+                        <a href="{{url('login')}}" id="login-link">Login
+                            {{-- <i class="fa fa-long-arrow-right"></i> --}}
+                        </a>
+                    </p>
+                </div>
+           </div>
+       </div>
+   </div>
+@stop
